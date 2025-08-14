f1505f4a8c02d868c857a2a7a595ac187b5f2cc1
AwanVisual <awanvisual27@gmail.com>
2025-08-14 11:39:49 +0700

    Updated Reports.tsx

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1252,13 +1252,7 @@ const Reports = () => {
                               >
                                 <Printer className="h-4 w-4" />
                               </Button>
-                              <Button
-                                size="sm"
-                                variant="outline"
-                                onClick={() => handleEditItems(sale)}
-                              >
-                                Edit Items
-                              </Button>
+                              
                             </>
                           )}
                         </div>
