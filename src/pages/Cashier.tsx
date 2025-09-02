e1491149b0e1e58e8945ce3cf12afaa97f4a43f7
AwanVisual <awanvisual27@gmail.com>
2025-09-02 19:24:55 +0700

    Updated Cashier.tsx

--- src/pages/Cashier.tsx
+++ src/pages/Cashier.tsx
@@ -981,7 +981,7 @@ const getEffectiveUnitPrice = (item: CartItem) => {
     }
 
 // ✅ Tambahkan tanda tangan
-yPosition += 2.0; // beri jarak ke bawah section terakhir
+yPosition += 0.0; // beri jarak ke bawah section terakhir
 checkPageBreak(2.0);
 pdf.setFontSize(10);
 pdf.setFont('helvetica', 'normal');
